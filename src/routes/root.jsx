cf5dc8bba54d73d95dcd7edb8664ed77e98f99c4
JoyontaSaha <joyontasaha8@gmail.com>
2023-05-27 18:01:02 +0600

    add synchronizing URLs to form state

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,6 +8,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
@@ -18,13 +19,16 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+  }, [q]);
 
     return (
       <>
@@ -38,6 +42,7 @@ export default function Root() {
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q}
               />
               <div
                 id="search-spinner"
