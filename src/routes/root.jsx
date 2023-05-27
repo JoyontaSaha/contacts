a4255747e50f76ec51e647807170ee3f17cbfde0
JoyontaSaha <joyontasaha8@gmail.com>
2023-05-27 13:16:43 +0600

    add active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,6 @@
 import {
   Outlet,
+  NavLink,
   Link,
   useLoaderData,
   Form,
@@ -52,16 +53,27 @@ export default function Root() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite && <span>★</span>}
-                    </Link>
+                    <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                    >
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite && <span>★</span>}
+                      </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
